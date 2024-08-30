485101127bc7be6fb7df766e77c58992026160c1
pallen1980 <69210558+pallen1980@users.noreply.github.com>
2024-08-30 23:49:08 +0100

    Upgraded Api to .Net v8.0 (#2)
    * DotNet Upgrade - Renamed/Archived Api Project
    
    * DotNet Upversioning - upped Api to .Net v8.0 + added https + swagger/openAPI

--- XeroProducts.Api/Models/Helpers.cs
+++ XeroProducts.Api/Models/Helpers.cs
@@ -1,15 +1,17 @@
-﻿using System.Data.SqlClient;
-using System.Web;
+﻿using Microsoft.Data.SqlClient;
+using System.Reflection;
 
 namespace XeroProducts.Models
 {
     public class Helpers
     {
         private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={DataDirectory}\Database.mdf;Integrated Security=True";
-
+        
         public static SqlConnection NewConnection()
         {
-            var connstr = ConnectionString.Replace("{DataDirectory}", HttpContext.Current.Server.MapPath("~/App_Data"));
+            //var connstr = ConnectionString.Replace("{DataDirectory}", System.Web.HttpContext.Current.Server.MapPath("~/App_Data"));
+            var connstr = ConnectionString.Replace("{DataDirectory}", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\App_Data");
+
             return new SqlConnection(connstr);
         }
     }
