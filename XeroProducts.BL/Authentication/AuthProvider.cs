d291ea49f2b79dbd20cbbe06dfdb33c3ce499517
Phil Allen <pallen1980@gmail.com>
2024-09-05 15:56:11 +0100

    Refactoring

--- XeroProducts.BL/Authentication/AuthProvider.cs
+++ XeroProducts.BL/Authentication/AuthProvider.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XeroProducts.BL.Interfaces;
 
-namespace XeroProducts.BL.Interfaces
+namespace XeroProducts.BL.Authentication
 {
     public class AuthProvider : IAuthProvider
     {
